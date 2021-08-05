96808d3a97d7b25e0fa687dd3037a087e7f2bc82
hieppn <hiep.bui21@student.passerellesnumeriques.org>
2021-08-05 14:21:49 +0700

    deploy

--- database/migrations/2021_08_03_092212_create_categories_table.php
+++ database/migrations/2021_08_03_092212_create_categories_table.php
@@ -17,7 +17,6 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->unique('name');
             $table->text('description');
             $table->timestamps();
         });
