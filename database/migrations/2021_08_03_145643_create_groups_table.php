74efd380480a2937af17881bafedf954e3177d8e
hieppn <hiep.bui21@student.passerellesnumeriques.org>
2021-08-05 14:27:17 +0700

    deploy

--- database/migrations/2021_08_03_145643_create_groups_table.php
+++ database/migrations/2021_08_03_145643_create_groups_table.php
@@ -16,7 +16,7 @@ class CreateGroupsTable extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name');
             $table->timestamps();
         });
     }
